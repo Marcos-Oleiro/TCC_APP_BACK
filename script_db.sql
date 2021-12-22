24d76cc9453cc6df1b2b856d9e583b7afdf097d9
Marcos-Oleiro <moleiro@ymail.com>
2021-12-21 22:40:18 -0300

    mudanca no script do banco

--- script_db.sql
+++ script_db.sql
@@ -2,7 +2,9 @@ DROP database tcc_app;
 
 CREATE database tcc_app;
 
-\c tcc_app;
+\ c tcc_app;
+
+CREATE EXTENSION earthdistance CASCADE;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
@@ -36,37 +38,130 @@ CREATE TABLE user_game (
     id_game integer NOT NULL REFERENCES games(id)
 );
 
-INSERT INTO games (name) VALUES ('jogo1');
-INSERT INTO games (name) VALUES ('jogo2');
-INSERT INTO games (name) VALUES ('jogo3');
+INSERT INTO
+    games (name)
+VALUES
+    ('jogo1');
 
-INSERT INTO users (nickname, passwd, email) VALUES ('spellzito','d3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69','spellzito@oleirosoftware.com.br');
-INSERT INTO users (nickname, passwd, email) VALUES ('user1','d3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69','user1@oleirosoftware.com.br');
-INSERT INTO users (nickname, passwd, email) VALUES ('user2','d3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69','user2@oleirosoftware.com.br');
-INSERT INTO users (nickname, passwd, email) VALUES ('user3','d3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69','user3@oleirosoftware.com.br');
-INSERT INTO users (nickname, passwd, email) VALUES ('user4','d3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69','user4@oleirosoftware.com.br');
-INSERT INTO users (nickname, passwd, email) VALUES ('user5','d3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69','user5@oleirosoftware.com.br');
-INSERT INTO users (nickname, passwd, email) VALUES ('user6','d3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69','user6@oleirosoftware.com.br');
-INSERT INTO users (nickname, passwd, email) VALUES ('user7','d3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69','user7@oleirosoftware.com.br');
-INSERT INTO users (nickname, passwd, email) VALUES ('user8','d3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69','user8@oleirosoftware.com.br');
+INSERT INTO
+    games (name)
+VALUES
+    ('jogo2');
+
+INSERT INTO
+    games (name)
+VALUES
+    ('jogo3');
+
+INSERT INTO
+    users (nickname, passwd, email)
+VALUES
+    (
+        'spellzito',
+        'd3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69',
+        'spellzito@oleirosoftware.com.br'
+    );
+
+INSERT INTO
+    users (nickname, passwd, email)
+VALUES
+    (
+        'user1',
+        'd3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69',
+        'user1@oleirosoftware.com.br'
+    );
+
+INSERT INTO
+    users (nickname, passwd, email)
+VALUES
+    (
+        'user2',
+        'd3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69',
+        'user2@oleirosoftware.com.br'
+    );
+
+INSERT INTO
+    users (nickname, passwd, email)
+VALUES
+    (
+        'user3',
+        'd3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69',
+        'user3@oleirosoftware.com.br'
+    );
+
+INSERT INTO
+    users (nickname, passwd, email)
+VALUES
+    (
+        'user4',
+        'd3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69',
+        'user4@oleirosoftware.com.br'
+    );
+
+INSERT INTO
+    users (nickname, passwd, email)
+VALUES
+    (
+        'user5',
+        'd3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69',
+        'user5@oleirosoftware.com.br'
+    );
+
+INSERT INTO
+    users (nickname, passwd, email)
+VALUES
+    (
+        'user6',
+        'd3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69',
+        'user6@oleirosoftware.com.br'
+    );
+
+INSERT INTO
+    users (nickname, passwd, email)
+VALUES
+    (
+        'user7',
+        'd3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69',
+        'user7@oleirosoftware.com.br'
+    );
+
+INSERT INTO
+    users (nickname, passwd, email)
+VALUES
+    (
+        'user8',
+        'd3d26f1e61ff157eb2e41f7ef2f6f47f3e67164e440eb27cfe2ee3a3d7e3cd69',
+        'user8@oleirosoftware.com.br'
+    );
 
 CREATE TABLE positions(
     id SERIAL PRIMARY KEY,
-	id_user INTEGER,
-	lat 	DOUBLE PRECISION, 
-	long	DOUBLE PRECISION,
-	time	TIMESTAMP DEFAULT NOW()	
+    id_user INTEGER,
+    lat NUMERIC(23, 20),
+    long NUMERIC(23, 20),
+    time TIMESTAMP DEFAULT NOW()
 );
 
-CREATE OR REPLACE FUNCTION delete_old_rows() RETURNS trigger
-    LANGUAGE plpgsql
-AS $$
-BEGIN
-    DELETE FROM positions WHERE time < CURRENT_TIMESTAMP - INTERVAL '1 hour';
-    RETURN NULL;
+CREATE
+OR REPLACE FUNCTION delete_old_rows() RETURNS trigger LANGUAGE plpgsql AS $ $ BEGIN
+DELETE FROM
+    positions
+WHERE
+    time < CURRENT_TIMESTAMP - INTERVAL '1 hour';
+
+RETURN NULL;
+
 END;
-$$;
+
+$ $;
 
 CREATE TRIGGER trigger_delete_old_rows
-    AFTER INSERT ON positions
-    EXECUTE PROCEDURE delete_old_rows();
+AFTER
+INSERT
+    ON positions EXECUTE PROCEDURE delete_old_rows();
+
+-- SELECT *,ROUND(earth_distance(ll_to_earth(-32.179610591118, -52.153114242071), ll_to_earth(lat, long)) :: NUMERIC,2) AS distance
+-- FROM positions WHERE earth_box(ll_to_earth (-32.179610591118, -52.153114242071), 10000) @> ll_to_earth (lat, long)
+--                  AND earth_distance(ll_to_earth (-32.179610591118, -52.153114242071), ll_to_earth (lat, long)) < 10000
+--                  AND earth_distance(ll_to_earth (-32.179610591118, -52.153114242071), ll_to_earth (lat, long)) > 0
+-- ORDER BY distance;
